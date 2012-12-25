9cd340059f4469c2a02b1d145b06084a2ee732d6
José Vidal <javidgon@gmail.com>
2012-12-25 17:02:56 +0100

    Create SMTP Support based on GMAIL
    Create SMTP support based on GMAIL in case of need of notification
    due to wrong response

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from werkzeug.wsgi import SharedDataMiddleware
 from endpoint import EndPoint
 from readers.yaml_reader import YamlReader
 
-def create_app(redis_host='localhost', redis_port=6379, with_static=True):
+def create_app(redis_host='localhost', redis_port=6379,
+               with_static=True, test_mode=False):
     reader = YamlReader('endpoint.yaml')
     # Set EndPoint object.
     app = EndPoint({
           'redis_host': redis_host,
-          'redis_port': redis_port
+          'redis_port': redis_port,
+          'test_mode': test_mode
     }, reader)
     if with_static:
         app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
--- settings.py
+++ settings.py
@@ -1,7 +1,16 @@
 import os
+import smtplib
 from jinja2 import Environment, FileSystemLoader
 
 TEMPLATE_PATH = os.path.join(os.path.dirname(__file__),
                                     'templates')
 JINJA_ENV = Environment(loader=FileSystemLoader(TEMPLATE_PATH),
                                      autoescape=True)
+# SMTP settings.
+SMTP_SERVER = smtplib.SMTP('smtp.gmail.com',587) #port 465 or 587
+SMTP_LOGIN = ''
+SMTP_PASSWORD = ''
+SMTP_FROM = ''
+SMTP_TO = ''
+SMTP_MSG = ''
+SMTP_STATUS = False
