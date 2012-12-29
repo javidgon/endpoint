6e087bc664a318a04d732fb9c7b5e270a0dbafe2
José Vidal <javidgon@gmail.com>
2012-12-29 16:01:52 +0100

    Create runners package
    In there you will find the run_app.py and run_mock.py files. Needed
    for running both the app and the mock

---
+++ runners/run_app.py
@@ -0,0 +1,24 @@
+import sys
+
+from endpoint.sentinels.sentinel import Sentinel
+from endpoint.runners.run_mock import create_mock
+from endpoint.readers.yaml_reader import YamlReader
+from endpoint.settings.settings import TESTING_YAML_PATH
+
+def create_app(test_mode=True, yalm_file=None):
+    reader = YamlReader(yalm_file)
+    sentinel = Sentinel({
+          'test_mode': test_mode
+    }, reader)
+
+    return sentinel
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    # Default yaml file if no params are passed.
+    yalm_file = TESTING_YAML_PATH
+    if len(sys.argv) > 1:
+        yalm_file = sys.argv[1]
+
+    app = create_app(yalm_file=yalm_file)
+    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
---
+++ runners/run_mock.py
@@ -0,0 +1,11 @@
+from endpoint.tests.mock import Mock
+
+def create_mock():
+    # Create Mock
+    return Mock()
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+
+    mock = create_mock()
+    run_simple('127.0.0.1', 5100, mock, use_debugger=True, use_reloader=True)
