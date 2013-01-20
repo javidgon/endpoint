d344443819088feff8c1b304f5109d9ad6ca165f
Jose Vidal <javidgon@gmail.com>
2013-01-20 14:54:24 +0100

    Rename run_app.py -> run_server.py

--- run_server.py
+++ run_server.py
@@ -8,5 +8,6 @@ def application():
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     from endpoint.settings import SERVER
-
+    
+    # Just for Development. Use WSGI instead in Production.
     run_simple(SERVER['host'], SERVER['port'], application(), use_debugger=True, use_reloader=True)
